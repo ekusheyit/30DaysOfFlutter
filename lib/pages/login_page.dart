315f5f0c6e1434100c36816dfb6bee5223f88512
DevTools <ahasansoft@gmail.com>
2021-04-28 14:26:28 +0600

    6day completed btn animated specifc time go to another page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,15 @@
 import 'package:first_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPageContent extends StatelessWidget {
+class LoginPageContent extends StatefulWidget {
+  @override
+  _LoginPageContentState createState() => _LoginPageContentState();
+}
+
+class _LoginPageContentState extends State<LoginPageContent> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -14,7 +22,7 @@ class LoginPageContent extends StatelessWidget {
           SizedBox(
             height: 20.0,
           ),
-          Text('Welcome to'),
+          Text("Welcome to $name"),
           SizedBox(
             height: 20.0,
           ),
@@ -27,6 +35,11 @@ class LoginPageContent extends StatelessWidget {
                   decoration: InputDecoration(
                     labelText: 'Username',
                   ),
+                  onChanged: (value) {
+                    setState(() {
+                      name = value;
+                    });
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
@@ -37,12 +50,37 @@ class LoginPageContent extends StatelessWidget {
                 SizedBox(
                   height: 20.0,
                 ),
-                ElevatedButton(
-                  onPressed: () {
+                InkWell(
+                  onTap: () async {
+                    setState(() {
+                      changeButton = true;
+                    });
+                    await Future.delayed(Duration(seconds: 1));
                     Navigator.pushNamed(context, MyRoutes.homeRoute);
                   },
-                  child: Text('Login'),
-                  style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    width: changeButton ? 50 : 150,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: changeButton
+                        ? Icon(
+                            Icons.done,
+                            color: Colors.white,
+                          )
+                        : Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                    ),
+                  ),
                 ),
               ],
             ),
